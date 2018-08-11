ecc2c143b9a2b6a85106c26f5aa3470297fcff40
Rajvir Rai <rsr1992@gmail.com>
2018-08-11 15:41:54 +0000

    automatically backed up by learn

--- example.rb
+++ example.rb
@@ -10,13 +10,13 @@ puts "Now, my age is #{my_age}"
 puts "Now, my age is " + my_age.to_s
 puts "My dogs name is #{dog}, my age is #{my_age} and I live in " + state
 puts dog + state
-
-def add_one_to_age
+def add_one_to_age(age)
   # body of function
   # what are we going to do here?
+  puts age
   my_age += 1
   puts "my new age is #{my_age}"
 end
 
 #calling, or invoking, a method:
-add_one_to_age
+add_one_to_age(30)
