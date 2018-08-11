cdda17e31add2478c89b2bcc11dc0345bffe68cb
Rajvir Rai <rsr1992@gmail.com>
2018-08-11 15:23:06 +0000

    automatically backed up by learn

--- example.rb
+++ example.rb
@@ -8,5 +8,5 @@ puts some_stuff_array
 my_age += 1 #same as my_age = my_age + 1
 puts "Now, my age is #{my_age}"
 puts "Now, my age is " + my_age.to_s
-puts "#{dog} + #{my_age}"
+puts "My dogs name is #{dog} + my age is #{my_age} and I live in" + state
 
