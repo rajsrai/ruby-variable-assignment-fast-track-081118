46d12b02999607666c91506dcc22b8946cd3a27a
Rajvir Rai <rsr1992@gmail.com>
2018-08-11 15:46:23 +0000

    automatically backed up by learn

--- example.rb
+++ example.rb
@@ -5,7 +5,7 @@ puts dog
 puts my_age
 puts some_stuff_array
 state = "FL"
-#my_age += 1 #same as my_age = my_age + 1
+my_age += 1 #same as my_age = my_age + 1
 puts "Now, my age is #{my_age}"
 puts "Now, my age is " + my_age.to_s
 puts "My dogs name is #{dog}, my age is #{my_age} and I live in " + state
